3815ba983b0c93a6bb946d0617721e4fc672e3eb
Marin Martinovic <mmartinovich@gmail.com>
2026-02-01 18:08:58 +0100

    Adedd Remotion skill

--- nuuky/components/ui/index.ts
+++ nuuky/components/ui/index.ts
@@ -1,9 +1,5 @@
 // UI Component Library
 // Reusable components for consistent design and reduced code duplication
 
-export { BaseModal } from './BaseModal';
 export { GradientButton } from './GradientButton';
-export { Avatar } from './Avatar';
-export { EmptyState } from './EmptyState';
-export { SettingsRow } from './SettingsRow';
 export { OTPInput } from './OTPInput';
